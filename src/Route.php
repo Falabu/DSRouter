6a43af488afcbe5275818ffb0adbd704f3d281ae
Kurucz Dávid <kurucz.david@biotechusa.com>
2019-04-04 09:24:52 +0200

    ADD: Route parameter binding
        -A routhoz-hoz rendelt osztaly/fuggveny-ben megkeresi a vart paramatereket es az alapjan hivja meg a fuggvenyt

--- src/Route.php
+++ src/Route.php
@@ -3,6 +3,7 @@
 
 namespace DSRouter;
 
+use ReflectionMethod;
 
 class Route
 {
@@ -10,40 +11,70 @@ class Route
 
     private $function_per_method;
 
+    private $parameter_name = 'valami';
+
+    private $parameter_value = 'ezkell nekem';
+
     public function __construct($route_name, $method, $function)
     {
         $this->route_name = $route_name;
         $this->function_per_method[$method] = $function;
     }
 
-    public function addCallFunctionToMethod($method, $function){
+    public function addCallFunctionToMethod($method, $function)
+    {
         $this->function_per_method[$method] = $function;
     }
 
-    public function getRouteName(){
+    public function getRouteName()
+    {
         return $this->route_name;
     }
 
-    public function methodExist($method){
-        if(isset($this->function_per_method[$method])){
+    public function methodExist($method)
+    {
+        if (isset($this->function_per_method[$method])) {
             return true;
         }
 
         return false;
     }
 
-    public function callFunction($method, Request $request){
+    public function callFunction($method, Request $request)
+    {
         list($class, $function) = explode('@', $this->function_per_method[$method]);
-        if(class_exists($class)){
-            $called_class = new $class();
 
-            if(method_exists($called_class, $function)){
-                $called_class->$function($request);
-            }else{
-                echo "Nincs ilyen hívható metódus az osztályban";
-                //TODO: Hibakezelés
+        if (class_exists($class)) {
+            $called_class = new $class();
+            $class_method_reflection = null;
+            $parameter_array = array();
+
+            try {
+                $class_method_reflection = new ReflectionMethod($called_class, $function);
+            } catch (\Exception $exception) {
+                echo $exception->getMessage();
+                //TODO:Hibakezelés
             }
-        }else{
+
+            if ($class_method_reflection) {
+                $class_method_parameters = $class_method_reflection->getParameters();
+
+                foreach ($class_method_parameters as $method_parameter) {
+                    if ($method_parameter->getClass()) {
+                        if ($method_parameter->getClass()->name === 'DSRouter\Request') {
+                            $parameter_array[] = $request;
+                        }
+                    }
+
+                    if ($method_parameter->name === $this->parameter_name) {
+                        $parameter_array[] = $this->parameter_value;
+                    }
+                }
+
+                $class_method_reflection->invokeArgs($called_class, $parameter_array);
+            }
+
+        } else {
             echo "Nincs ilyen hívható osztály ami a útvonalhoz van kapcsolva";
             //TODO: Hibakezelés
         }
